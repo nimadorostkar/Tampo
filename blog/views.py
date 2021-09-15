c6b0d56a7fc95ae89ca4e00619705e5d18e59537
nimadorostkar <nimadorostkar97@gmail.com>
2021-09-15 12:54:37 +0430

    Contact form in tampo.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from . import models
-from .models import Post
-from .forms import CommentForm, NewsletterForm
+from .models import Post, Contact
+from .forms import CommentForm, NewsletterForm, ContactForm
 from django.shortcuts import render, get_object_or_404
 
 
@@ -54,18 +54,23 @@ def post_detail(request, slug):
 def tampo(request):
     posts = models.Post.objects.filter(status=True).order_by("-created_on")[:3]
 
-
-
-
     if request.method == "POST":
         newsletter_form = NewsletterForm(data=request.POST)
+        contact_form = ContactForm(data=request.POST)
         if newsletter_form.is_valid():
             obj = newsletter_form.save(commit=False)
             obj.save()
+        if contact_form.is_valid():
+            obj = Contact()
+            obj.name = contact_form.cleaned_data['name']
+            obj.email = contact_form.cleaned_data['email']
+            obj.body = contact_form.cleaned_data['body']
+            obj.save()
     else:
         newsletter_form = NewsletterForm()
+        contact_form = ContactForm(data=request.POST)
 
-    return render(request, 'tampo.html', {'posts': posts, 'newsletter_form':newsletter_form})
+    return render(request, 'tampo.html', {'posts': posts, 'newsletter_form':newsletter_form, 'contact_form':contact_form})
 
 
 
